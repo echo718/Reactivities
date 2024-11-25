9a7c37a9284bccdf014797c5e9c3232b4f905bce
echo718 <suntongecho718@gmail.com>
2024-11-26 11:20:33 +1300

    create followers api;

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3799e6fca1fe536570c103a467ffec7e78feac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d297139d63dbf9f0d0eb2e8c81fe02eb9bbb5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
